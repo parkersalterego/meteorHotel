35d5457dad163dc69a9ebbf91c89951e0ab0edf5
parkersalterego <parkersalterego92@gmail.com>
2018-07-21 14:55:27 -0700

    pulled in aldeed/collection2-core via atmosphere and simplschema via npm. Schemas created for Rooms and Members

--- imports/api/rooms.js
+++ imports/api/rooms.js
@@ -1,5 +1,27 @@
 import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'simpl-schema';
+
+SimpleSchema.extendOptions(['autoform']);
 
 const Rooms = new Mongo.Collection('rooms');
+
+const RoomSchema = new SimpleSchema({
+  roomNumber: { type: Number },
+  checkIn: { type: Date },
+  checkout: { type: Date },
+  tenantId: { type: String },
+  available: { type: Boolean },
+  needsCleaning: { type: Boolean },
+  CreatedAt: {
+    type: Date,
+    autoform: {
+      type: 'hidden',
+      label: false,
+    },
+    defaultValue: new Date(),
+  },
+});
+
+Rooms.attatchSchema(RoomSchema);
 
 export default Rooms;
